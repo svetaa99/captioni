42cd70040b5633ed7d598a224b3ece9557e8cdba
svetaa99 <svetozar.vulin@gmail.com>
2021-06-30 16:34:52 +0200

    Implemented data generator for preparing train data.

--- training_caption_generator.py
+++ training_caption_generator.py
@@ -158,22 +158,50 @@ def calculate_max_length(descriptions):
     return max(len(d.split()) for d in desc_list)
 
 
+def data_generator(descriptions, features, tokenizer, max_length):
+    while 1:
+        for key, description_list in descriptions.items():
+            feature = features[key][0]
+            input_image, input_sequence, output_word = create_sequences(tokenizer, max_length, description_list, feature)
+
+            yield [[input_image, input_sequence], output_word]
+
+
+def create_sequences(tokenizer, max_length, desc_list, feature):
+    X1, X2, y = list(), list(), list()
+    for desc in desc_list:
+        # encode the sequence
+        seq = tokenizer.texts_to_sequences([desc])[0]
+        # split one sequence into multiple X, y pairs
+        for i in range(1, len(seq)):
+            # split into input and output pair
+            in_seq, out_seq = seq[:i], seq[i]
+            in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+            # encode output sequence
+            out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+            # store
+            X1.append(feature)
+            X2.append(in_seq)
+            y.append(out_seq)
+    return np.array(X1), np.array(X2), np.array(y)
+
 if __name__ == "__main__":
     # CREATING DESCRIPTION.TXT AND CLEANING TOKEN.TXT FILE
 
     tokens_file = dataset_text + "/" + "Flickr8k.token.txt"
 
-    captions = all_img_captions(tokens_file)
-    captions = clean_text(captions)
+    descriptions = all_img_captions(tokens_file)
+    descriptions = clean_text(descriptions)
 
-    vocabulary = create_text_vocabulary(captions)
+    vocabulary = create_text_vocabulary(descriptions)
 
-    save_descriptions(captions)
+    save_descriptions(descriptions)
 
     # EXTRACTING FEATURES WITH XCEPTION MODEL
 
     # features = extract_features(dataset_images)
     # dump(features, open("features.p", "wb"))
+    features = load(open("features.p", "rb"))
 
     # LOADING TRAINING DATASET
 
@@ -188,4 +216,9 @@ if __name__ == "__main__":
     dump(tokenizer, open("tokenizer.p", "wb"))
     vocab_size = len(tokenizer.word_index) + 1
 
-    max_lengt = calculate_max_length(train_descriptions)
+    max_length = calculate_max_length(descriptions)
+
+    # DATA GENERATOR
+
+    [a, b], c = next(data_generator(train_descriptions, features, tokenizer, max_length))
+
